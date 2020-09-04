fbdf0b27e51fd3423f4cad918e74ae10d32fe116
ontunae <70752843+ontunae@users.noreply.github.com>
2020-09-04 19:19:48 +0900

    Update main.py

--- main.py
+++ main.py
@@ -3,14 +3,7 @@ import telegram
 from apscheduler.schedulers.blocking import BlockingScheduler
 
 
-#진짜: 1372299266:AAHY6XiAyzzp5Hw54CzgzZ4csQzNGNhRi-w
-#진짜: 1215804711
-
-#테스트: 1261968915:AAENDB7nRG5_zLFcqrkkaaJwfepo-F07BYQ
-#테스트:1215804711
-
-bot = telegram.Bot(token='1372299266:AAHY6XiAyzzp5Hw54CzgzZ4csQzNGNhRi-w')
-#test = telegram.Bot(token='1261968915:AAENDB7nRG5_zLFcqrkkaaJwfepo-F07BYQ')
+bot = telegram.Bot(token='TELEGRAM_TOKEN')
 
 
 def currentNum():
@@ -19,10 +12,7 @@ def currentNum():
     }
 
     params = (
-        #고강방1
-        ('query', 'https://open.kakao.com/o/gYSBYqF'),
-        #캐럿방
-        #('query', 'https://open.kakao.com/o/gVIIm6Yb'),
+        ('query', 'OPENCHAT_LINK'),
         ('type', 'm'),
         ('page', '1'),
         ('count', '30'),
@@ -32,12 +22,8 @@ def currentNum():
     result = response.json()['result']['lists'][0]
     desc = result['desc']
 
-    #test.sendMessage(chat_id=1215804711, text='10초 간격 체크 가능 여부 테스트입니다. 양해 바랍니다.')
-    if result['headcount'] < 1500:
-        #진짜
-        bot.sendMessage(chat_id=1215804711, text=result['name'] + '\n에 자리가 났습니다! 서두르세요!!\n' + desc[:desc.find(']') + 1])
-        #테스트
-        #test.sendMessage(chat_id=1215804711, text=result['name'] + '\n에 자리가 났습니다! 서두르세요!!')
+    if result['headcount'] < OPENCHAT_MAX_HEADCOUNT:
+        bot.sendMessage(chat_id=CHAT_ID, text=result['name'] + '\n에 자리가 났습니다! 서두르세요!!\n' + desc[:desc.find(']') + 1])
 
 
 sched = BlockingScheduler()
